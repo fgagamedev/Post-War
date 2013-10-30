f9d7728c425f5ff54485edcc6335fd777b6656f8
Bruno Rodrigues <brunounbgama@gmail.com>
2013-10-30 11:48:05 -0200

    Alterei os créditos

--- source/GameFeatures/Creditos/creditos.cpp
+++ source/GameFeatures/Creditos/creditos.cpp
@@ -16,26 +16,46 @@ void creditos(SDL_Surface *screen){
 
     string tela = "source/GameFeatures/Creditos/Images/tela_preta.png";
     SDL_Surface *telapreta = load_Image(tela, coisa);
+    string imagem_bryj = "source/GameFeatures/Creditos/Images/1.png";
+    SDL_Surface *bryj = load_Image(imagem_bryj, coisa);
     string imagem_sdl = "source/GameFeatures/Creditos/Images/2.png";
     SDL_Surface *sdl = load_Image(imagem_sdl, coisa);
     string imagem_ubuntu = "source/GameFeatures/Creditos/Images/3.png";
     SDL_Surface *ubuntu = load_Image(imagem_ubuntu, coisa);
+    string imagem_16 = "source/GameFeatures/Creditos/Images/4.png";
+    SDL_Surface *dezesseis= load_Image(imagem_16, coisa);
+    string creditos_path = "source/GameFeatures/Creditos/Images/creditos.png";
+    SDL_Surface *creditos= load_Image(creditos_path, coisa);
+
 
     SDL_Flip(screen);
 
 
-    for(int i = -680; i <= 680; i++){
-		BlitImage(screen, sdl, 0, i);
-		SDL_Flip(screen);
-		BlitImage(screen, telapreta, 0, 0);
-		SDL_Flip(screen);
-		if(i==680){
-            for(int j = -680; j <= 680; j++){
-                BlitImage(screen, ubuntu, 0, j);
-                SDL_Flip(screen);
-            }
-		}
+    for(int j = -680; j <= 680; j+=2){
+        BlitImage(coisa, telapreta, 0, 0);
+        BlitImage(coisa, creditos, 0, j);
+        SDL_Flip(coisa);
     }
-		SDL_Delay(2000);
+
+    for(int i = -680; i <= 680; i+=2){
+		BlitImage(coisa, telapreta, 0, 0);
+		BlitImage(coisa, bryj, 0, i);
+		SDL_Flip(coisa);
     }
+
+
+    for(int i = -680; i <= 680; i+=2){
+		BlitImage(coisa, telapreta, 0, 0);
+		BlitImage(coisa, sdl, 0, i);
+		SDL_Flip(coisa);
+    }
+
+    for(int j = -680; j <= 680; j+=2){
+        BlitImage(coisa, telapreta, 0, 0);
+        BlitImage(coisa, ubuntu, 0, j);
+        SDL_Flip(coisa);
+    }
+
+
+}
 
