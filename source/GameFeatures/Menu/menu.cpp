100fce7183704863203bc6bc0f1e33bd5d7e9882
Bruno Rodrigues <brunounbgama@gmail.com>
2013-10-30 00:09:42 -0200

    Todas as imagens de seleção do menu estão implementadas

--- source/GameFeatures/Menu/menu.cpp
+++ source/GameFeatures/Menu/menu.cpp
@@ -12,17 +12,30 @@ void load_menu(SDL_Surface *screen){
     SDL_Surface *jogar_selecionado;
     SDL_Surface *creditos_selecionado;
     SDL_Surface *sair_selecionado;
+    SDL_Surface *estatisticas_selecionado;
+    SDL_Surface *opcoes_selecionado;
     //carregando imagens
     string path_jogo = "source/GameFeatures/Menu/Images/jogar_selecionado.png";
     jogar_selecionado = load_Image(path_jogo,screen);
+
+    string path_estatisticas = "source/GameFeatures/Menu/Images/estatisticas_selecionado.png";
+    estatisticas_selecionado = load_Image(path_estatisticas, screen);
+
     string path_creditos = "source/GameFeatures/Menu/Images/creditos_selecionado.png";
     creditos_selecionado = load_Image(path_creditos, screen);
+
     string path_sair = "source/GameFeatures/Menu/Images/sair_selecionado.png";
     sair_selecionado = load_Image(path_sair, screen);
+
+    string path_opcoes = "source/GameFeatures/Menu/Images/opcoes_selecionado.png";
+    opcoes_selecionado = load_Image(path_opcoes, screen);
+
     string path = "source/GameFeatures/Menu/Images/menu.png";
     SDL_Surface *menu = load_Image(path, screen);
 
 
+
+    BlitImage(screen, menu, 0, 0);
     SDL_Flip(screen);
     int cont=0;
     Vetor_mouse *vetor = new Vetor_mouse;
@@ -48,34 +61,55 @@ void load_menu(SDL_Surface *screen){
             cont=0;
         }
 
-            else if(compara_selecao(425,798,vetor->x,511,575,vetor->y)){
-                BlitImage(screen, creditos_selecionado, 425.0, 511);
+            else if(compara_selecao(450,798,vetor->x,311,375,vetor->y)){
+                BlitImage(screen, opcoes_selecionado, 450.0, 311);
                 SDL_Flip(screen);
-                //cout<<"Entrei aqui"<<endl;
                 if(vetor->click == 1){
-                    //cout<<"Clicou em jogar"<<endl;
-                    creditos(screen);
+                                //cout<<"Clicou em jogar"<<endl;
                 }
                 cont=0;
-            }
-
-                else if(compara_selecao(350,798,vetor->x,611,675,vetor->y)){
-                    BlitImage(screen, sair_selecionado, 350.0, 611);
-                    SDL_Flip(screen);
-                    //cout<<"Entrei aqui"<<endl;
-                    if(vetor->click == 1){
-                        //cout<<"Clicou em jogar"<<endl;
-                        creditos(screen);
-                    }
-                    cont=0;
                 }
+                    else if(compara_selecao(355,798,vetor->x,417,475,vetor->y)){
+                            BlitImage(screen, estatisticas_selecionado, 355.0, 411);
+                            SDL_Flip(screen);
+                            //cout<<"Entrei aqui"<<endl;
+                            if(vetor->click == 1){
+                                //cout<<"Clicou em jogar"<<endl;
+                            }
+                            cont=0;
+                        }
+
+                        else if(compara_selecao(425,798,vetor->x,511,575,vetor->y)){
+                            BlitImage(screen, creditos_selecionado, 425.0, 511);
+                            SDL_Flip(screen);
+                            //cout<<"Entrei aqui"<<endl;
+                            if(vetor->click == 1){
+                                //cout<<"Clicou em jogar"<<endl;
+                                creditos(screen);
+                            }
+                            cont=0;
+                        }
+
+                                else if(compara_selecao(350,798,vetor->x,611,675,vetor->y)){
+                                    BlitImage(screen, sair_selecionado, 350.0, 611);
+                                    SDL_Flip(screen);
+                                    //cout<<"Entrei aqui"<<endl;
+                                    if(vetor->click == 1){
+                                        //cout<<"Clicou em jogar"<<endl;
+                                        SDL_Quit();
+                                        exit(0);
+                                    }
+                                    cont=0;
+                                }
 
 
-                else if (cont==0){
-                    BlitImage(screen, menu, 0, 0);
-                    SDL_Flip(screen);
-                    cont=1;
-            }
+
+
+                                else if (cont==0){
+                                    BlitImage(screen, menu, 0, 0);
+                                    SDL_Flip(screen);
+                                    cont=1;
+                                }
     }
 
     SDL_Delay(10000);
