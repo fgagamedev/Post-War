03a2d960f1dfd6f4e3123b9a98a50f3075050bfb
Yago <yago@yago-Inspiron-3421.(none)>
2013-10-30 00:00:18 -0200

    Começando a mostrar creditos

--- source/GameFeatures/Creditos/creditos.cpp
+++ source/GameFeatures/Creditos/creditos.cpp
@@ -7,7 +7,6 @@
 #include "../../../include/gamefeatures.h"
 
 
-
 using namespace std;
 
 void creditos(SDL_Surface *screen){
@@ -16,8 +15,23 @@ void creditos(SDL_Surface *screen){
 
     string imagem_sdl = "source/GameFeatures/Creditos/Images/2.png";
     SDL_Surface *sdl = load_Image(imagem_sdl, coisa);
+    string imagem_ubuntu = "source/GameFeatures/Creditos/Images/3.png";
+    SDL_Surface *ubuntu = load_Image(imagem_ubuntu, coisa);
 
-    SDL_Flip(coisa);
+    SDL_Flip(screen);
+
+
+    for(int i = -680; i <= 680; i++){
+		BlitImage(coisa, sdl, 0, i);
+		SDL_Flip(coisa);
+		if(i==680){
+            for(int j = -680; j <= 680; j++){
+                BlitImage(screen, ubuntu, 0, j);
+                SDL_Flip(screen);
+            }
+		}
+    }
+
 
     SDL_Delay(2000);
 
