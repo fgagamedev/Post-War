9bb8b7f02d25364738a6706158e143d7201aaf93
Yago <yago@yago-Inspiron-3421.(none)>
2013-10-30 00:24:23 -0200

    Do mesmo jeito ainda

--- source/GameFeatures/Creditos/creditos.cpp
+++ source/GameFeatures/Creditos/creditos.cpp
@@ -9,10 +9,13 @@
 
 using namespace std;
 
+
 void creditos(SDL_Surface *screen){
 
     SDL_Surface *coisa = setup_video();
 
+    string tela = "source/GameFeatures/Creditos/Images/tela_preta.png";
+    SDL_Surface *telapreta = load_Image(tela, coisa);
     string imagem_sdl = "source/GameFeatures/Creditos/Images/2.png";
     SDL_Surface *sdl = load_Image(imagem_sdl, coisa);
     string imagem_ubuntu = "source/GameFeatures/Creditos/Images/3.png";
@@ -22,8 +25,10 @@ void creditos(SDL_Surface *screen){
 
 
     for(int i = -680; i <= 680; i++){
-		BlitImage(coisa, sdl, 0, i);
-		SDL_Flip(coisa);
+		BlitImage(screen, sdl, 0, i);
+		SDL_Flip(screen);
+		BlitImage(screen, telapreta, 0, 0);
+		SDL_Flip(screen);
 		if(i==680){
             for(int j = -680; j <= 680; j++){
                 BlitImage(screen, ubuntu, 0, j);
@@ -31,8 +36,6 @@ void creditos(SDL_Surface *screen){
             }
 		}
     }
+		SDL_Delay(2000);
+    }
 
-
-    SDL_Delay(2000);
-
-}
