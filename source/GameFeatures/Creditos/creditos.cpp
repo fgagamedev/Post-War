4ed0352193103c6fc601049a58bc2db82142613e
Yago <yago@yago-Inspiron-3421.(none)>
2013-10-29 23:29:00 -0200

    Opcao credtios clicavel

--- source/GameFeatures/Creditos/creditos.cpp
+++ source/GameFeatures/Creditos/creditos.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
-#include<SDL/SDL.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "../../../include/draw.h"
+#include "../../../include/video.h"
+#include "../../../include/loop.h"
+#include "../../../include/gamefeatures.h"
+
+
 
 using namespace std;
 
 void creditos(SDL_Surface *screen){
 
-    cout<<"Creditos aew"<<endl;
+    SDL_Surface *coisa = setup_video();
+
+    string imagem_sdl = "source/GameFeatures/Creditos/Images/2.png";
+    SDL_Surface *sdl = load_Image(imagem_sdl, coisa);
+
+    SDL_Flip(coisa);
+
+    SDL_Delay(2000);
 
 }
