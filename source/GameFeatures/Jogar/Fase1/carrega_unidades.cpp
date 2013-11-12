3a3f2f10dc05d7e345f34aa92b9d7ea806306206
Bruno Rodrigues <brunounbgama@gmail.com>
2013-11-12 18:27:14 -0200

    Adicionei metralhadora azul na china

--- source/GameFeatures/Jogar/Fase1/carrega_unidades.cpp
+++ source/GameFeatures/Jogar/Fase1/carrega_unidades.cpp
@@ -29,13 +29,14 @@ void setar_helicoptero(Unidade *helicoptero, char cor[], char tipo[]){
     helicoptero->cor = cor;
 }
 
-void setar_metralhadora(Unidade *metralhadora, char cor[], char tipo[]){
+void setar_metralhadora(Unidade *metralhadora, char cor[], char tipo[],SDL_Surface *nome){
     metralhadora->hp = 100;
     metralhadora->atk = 200;
     metralhadora->def = 300;
     metralhadora->alcance = 3;
     metralhadora->ouro = 200;
     metralhadora->cor = cor;
+    metralhadora->nome = nome;
 }
 
 void setar_tanque(Unidade *tanque, char cor[], char tipo[]){
@@ -50,21 +51,32 @@ void setar_tanque(Unidade *tanque, char cor[], char tipo[]){
 void carrega_china(SDL_Surface *screen,string lado){
 
     string lado1("servidor");
-
+    char cor[100] = "azul";
     for(int i=0, j=1;i<10;i++,j+=30){
         string caminho = "source/GameFeatures/Jogar/Fase1/images/unidade_azul_direita1.png";
         SDL_Surface *soldado1 = load_Image(caminho, screen);
         Unidade *soldado = new Unidade();
-        char cor[100] = "azul";
+
         char unidade[100] = "soldado";
         setar_soldado(soldado,cor,unidade,soldado1);
         unidades_azul.push_back(soldado);
     }
 
-    for(int i=0, j =1;i<10;i++,j+=30){
+
+    string caminho = "source/GameFeatures/Jogar/Fase1/images/metralha_azul.png";
+    SDL_Surface *metralhadora1 = load_Image(caminho, screen);
+    Unidade *metralhadora = new Unidade();
+    char tipo[100] = "metralhadora";
+    setar_metralhadora(metralhadora,cor,tipo,metralhadora1);
+    unidades_azul.push_back(metralhadora);
+
+
+    for(int i=0, j =1;i<11;i++,j+=30){
+            cout<<"aia"<<endl;
             BlitImage(screen,unidades_azul[i]->nome,i+200,j+150   );
 
-        }
+    }
+
 
 
 }
@@ -97,7 +109,7 @@ void carrega_eua(SDL_Surface *screen,string lado){
         Unidade *metralhadora = new Unidade();
         char cor[100] = "vermelha";
         char unidade[100] = "metralhadora";
-        setar_metralhadora(metralhadora, cor, unidade);
+        setar_metralhadora(metralhadora, cor, unidade,metrapalhadora1);
     }
 
      for (int i=0;i<=0;i++){
