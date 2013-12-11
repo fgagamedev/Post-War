abde4e121aab31d08a7d6069290ad0cf4508374b
JonathanRufino <jonathan_rufinotkd@hotmail.com>
2013-12-11 04:04:26 -0200

    opcoes arrumado

--- source/GameFeatures/Opcoes/opcoes.cpp
+++ source/GameFeatures/Opcoes/opcoes.cpp
@@ -23,11 +23,13 @@ void opcoes(SDL_Surface *screen){
 
     BlitImage(screen, telafundo, 0, 0);
     Vetor_mouse *vetor = new Vetor_mouse;
-
+	int Mix_Volume(int channel, int volume2);
+	//int Mix_VolumeMusic(int volume2);
     start=0;
     float volume_menos = 5;
     float volume_mais = 5;
 	int volume=0;
+	int volume2=128;
 
     while(1){
 
@@ -35,18 +37,20 @@ void opcoes(SDL_Surface *screen){
         vetor = get_Input();
 		
 		if(volume_mais){
-		BlitImage(screen, imagemBarra, 750-(volume*10), 343);
+			BlitImage(screen, imagemBarra, 750-(volume*10), 343);
 		}
-			else if (volume_menos){
-			BlitImage(screen, imagemBarra, 750+(volume*10), 343);
-		}   
+		else if (volume_menos){
+				BlitImage(screen, imagemBarra, 750+(volume*10), 343);
+			}   
 
         //Botao X da tela
         if(compara_selecao(792, 822, vetor->x, 296, 324, vetor->y)){
             if(vetor->click == 1){
-				play_effect(efeito_click);                
+				Mix_VolumeChunk(efeito_selecao_cancelar, 20);
+				play_effect(efeito_selecao_cancelar);   
+				SDL_Delay(250);             
 				break;
-            }
+            }	
         }
             //Botao - do volume
             else if (compara_selecao(486, 506, vetor->x, 356, 378, vetor->y)){
@@ -55,10 +59,13 @@ void opcoes(SDL_Surface *screen){
                 if(vetor->click == 1){
 					play_effect(efeito_click); 
                     // set the music volume to 1/2 maximum, and then check it
-                    printf("volume was    : %d\n", Mix_VolumeMusic(128-volume_menos));
+                    //printf("volume was    : %d\n", Mix_VolumeMusic(128-volume_menos));
                     //printf("volume is now : %d\n", Mix_VolumeMusic(-1));
-
-					volume_menos = volume_menos + 5;
+					volume2=volume2-5;
+					Mix_Volume(-1, volume2);
+					Mix_VolumeMusic(volume2);
+					//printf("volume is now : %d\n", Mix_Volume(0, volume2));
+					//volume_menos = volume_menos + 5;
 					volume++;
 					BlitImage(screen, telafundo, 0, 0);
 					
@@ -69,10 +76,13 @@ void opcoes(SDL_Surface *screen){
                     if(vetor->click == 1){
 						play_effect(efeito_click); 
                         // set the music volume to 1/2 maximum, and then check it
-                        printf("volume was    : %d\n", Mix_VolumeMusic(Mix_VolumeMusic(-1)+5));
-                        printf("volume is now : %d\n", Mix_VolumeMusic(-1));
-
-                    	volume_menos = volume_menos - 5;
+                        //printf("volume was    : %d\n", Mix_VolumeMusic(Mix_VolumeMusic(-1)+5));
+                        //printf("volume is now : %d\n", Mix_VolumeMusic(-1));
+						volume2=volume2+5;
+						Mix_Volume(-1, volume2);
+						Mix_VolumeMusic(volume2);
+						//printf("volume is now : %d\n", Mix_Volume(0, volume2));
+                    	//volume_menos = volume_menos - 5;
 						volume--;
 						BlitImage(screen, telafundo, 0, 0);
                     }
